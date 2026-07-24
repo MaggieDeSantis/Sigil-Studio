3b2a768dcc0e1da4f5280481c3e7a74bfa6ebf3e
Maggie DeSantis <sheisntreal@gmail.com>
2026-07-24 16:07:05 -0400

    learned about top, bottom, left and right points on the circle

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("sigilCanvas");
 const ctx = canvas.getContext("2d");
 
+const centerX = 250;
+const centerY = 250;
+
 const button = document.getElementById("generateBtn");
 
 button.addEventListener("click", drawCircle);
@@ -12,11 +15,33 @@ function drawCircle(){
     ctx.beginPath();
     ctx.arc(250, 250, 100, 0, Math.PI * 2);
     ctx.lineWidth = 4;
-
     ctx.stroke();
+
+//top point
+    ctx.beginPath(),
+    ctx.arc(centerX, centerY - 100, 5, 0, Math.PI * 2);
+    ctx.fill();
 
 //center
     ctx.beginPath();
-    ctx.arc(250, 250, 3, 0, Math.PI * 2);
+    ctx.arc(centerX, centerY, 3, 0, Math.PI * 2);
     ctx.fill();
+
+//bottom point
+    ctx.beginPath();
+    ctx.arc(centerX, centerY + 100, 5, 0, Math.PI * 2);
+    ctx.fill();
+
+// left point 
+    ctx.beginPath();
+    ctx.arc(centerX - 100, centerY, 5, 0, Math.PI * 2);
+    ctx.fill();
+
+//right point 
+
+    ctx.beginPath();
+    ctx.arc(centerX + 100, centerY, 5, 0, Math.PI * 2);
+    ctx.fill();
+
 }
+
